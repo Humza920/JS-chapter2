8761b87d440567075cf8b649f378d7bf37eb0fcd
Humza920 <humza.rehman.779@gmail.com>
2024-02-27 01:54:05 -0800

    CREATED

---
+++ app.js
@@ -0,0 +1,41 @@
+// CHAPTER 2
+// Question 1
+
+var userName = ("");
+
+
+// Question 2
+
+var myName = ("Humza Rehman");
+
+// Question 3
+
+var message = ("Hello World!");
+
+alert(message)
+
+// Question 4
+
+var firstName = ("John Doe")
+alert(firstName)
+
+var age = ("i am 15 years old")
+alert(age)
+
+var earn = ("Certified Mobile Application Development")
+alert(earn)
+
+// Question 5
+
+var pizza =("Pizza\nPizz\nPiz\nPi\nP")
+alert(pizza)
+
+// Question 6
+
+var email = ("example@example.com")
+alert("My email is" + " " + email)
+
+// Question 7
+
+var book = ("A smarter way to lerarn Java Script")
+alert("I am trying to learn from a Book " + book)
